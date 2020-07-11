344ce79ab385eb5c7f353d1c71885a7fb3b438bd
Lisheng Zheng <zls1129@gmail.com>
2020-07-12 00:24:40 +0800

    dp 1510

---
+++ src/n1510_stone_game_iv.go
@@ -0,0 +1,23 @@
+// 一个比较典型的 dp 问题，从 1 到 n 每次都把结果算出来，
+// 计算到 n+1 的时候第一步尝试放小于等于 n 的所有 square 数量，
+// 因为对手的放的 stone 肯定是算过的了，所以看下对手能不能赢，自己的结果
+// 取反就好了！！！
+func winnerSquareGame(n int) bool {
+	dp := make([]bool, n+1)
+	dp[1] = true
+	lastSquare := 2
+	for i := 2; i <= n; i++ {
+		if lastSquare*lastSquare == i {
+			dp[i] = true
+			lastSquare += 1
+			continue
+		}
+		for j := 1; j < lastSquare; j++ {
+			if !dp[i-j*j] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[n]
+}
